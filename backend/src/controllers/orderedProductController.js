8b6627f7b2ce4f6068e3e82e5db6bafabcff1c7f
Magno Rocha da Cruz <mcruz@cognyhub.com>
2024-02-22 17:56:29 -0300

    layout frontend inicial

--- backend/src/controllers/orderedProductController.js
+++ backend/src/controllers/orderedProductController.js
@@ -1,34 +1,35 @@
-const orderedProductModel = require('../models/orderedProductModel');
-const app = require('../app');
-
+const orderedProductModel = require("../models/orderedProductModel");
+const app = require("../app");
 
 const getOrderedProduct = async (req, res) => {
-    const pedidos = await orderedProductModel.getOrderedProduct();
-    return res.status(200).json(pedidos);
+  const pedidos = await orderedProductModel.getOrderedProduct();
+  return res.status(200).json(pedidos);
 };
 
 const addOrderedProduct = async (request, res) => {
-    const createdOrder = await orderedProductModel.addOrderedProduct(request.body);
-    return res.status(201).json(request.body);
-}; 
+  const createdOrderedProduct = await orderedProductModel.addOrderedProduct(
+    request.body
+  );
+  return res.status(201).json(request.body);
+};
 
 const deleteOrderedProduct = async (request, res) => {
-    const { id } = request.params;
+  const { id } = request.params;
 
-    await orderedProductModel.deleteOrderedProduct(id);
-    return res.status(204).json();
-}
+  await orderedProductModel.deleteOrderedProduct(id);
+  return res.status(204).json();
+};
 
 const updateOrderedProduct = async (request, res) => {
-    const { id } = request.params;
+  const { id } = request.params;
 
-    await orderedProductModel.updateOrderedProduct(id, request.body);
-    return res.status(204).json();
-}
+  await orderedProductModel.updateOrderedProduct(id, request.body);
+  return res.status(204).json();
+};
 
 module.exports = {
-    getOrderedProduct,
-    addOrderedProduct,
-    deleteOrderedProduct,
-    updateOrderedProduct,
+  getOrderedProduct,
+  addOrderedProduct,
+  deleteOrderedProduct,
+  updateOrderedProduct,
 };
